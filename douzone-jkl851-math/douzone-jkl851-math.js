ac82fcbf4a01b7da400f35e642dac0e7cff89425
jkl851 <jkl851@naver.com>
2021-11-15 16:24:25 +0900

    http module #1

--- douzone-jkl851-math/douzone-jkl851-math.js
+++ douzone-jkl851-math/douzone-jkl851-math.js
@@ -1,28 +1,26 @@
-module.exports= {
-    sum : function() {
+module.exports = {
+    sum: function() {
         var sum = 0;
-        // Array.prototype.forEach(arguments)
-        Array.from(arguments).forEach(function(e) {
+        Array.from(arguments).forEach(function(e){
             sum += e;
         });
-       
+
         return sum;
     },
     max: function() {
-        var max = Number.MAX_SAFE_INTEGER;
-        Array.from(arguments).forEach(function(e) {
+        var max = Number.MIN_SAFE_INTEGER;
+        Array.from(arguments).forEach(function(e){
             max = e > max ? e : max;
         });
 
-        return max;
+        return max
     },
-    min : function() {
+    min: function() {
         var min = Number.MAX_SAFE_INTEGER;
-        Array.from(arguments).forEach(function(e) {
+        Array.from(arguments).forEach(function(e){
             min = e < min ? e : min;
-        })
+        });
 
-        return min;
+        return min
     }
 }
-
