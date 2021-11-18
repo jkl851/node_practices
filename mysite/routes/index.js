952e5d13f845fbeeb0a2ff7cc68b05632c8666f9
jkl851 <jkl851@naver.com>
2021-11-18 14:19:10 +0900

    index route #1

---
+++ mysite/routes/index.js
@@ -0,0 +1,24 @@
+const errorRoute = require('./error');
+
+const applicationRouter = {
+    setup: function(application) {
+        
+        //const site = models.Site.findOne();
+
+        application
+            .all('*', function(req, res, next){
+            res.locals.req = req;
+            res.locals.res = res;
+            next();
+        })
+        .use('/', require('./main'))
+        .use('/user',require('./user'))
+
+        .use(errorRoute.error404)
+        .use(errorRoute.error500)
+
+        .siteTitle = 'MySite';
+    }
+}
+
+module.exports = { applicationRouter }
