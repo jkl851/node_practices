9edf7075c075638e9f027de11f52e80581c14eb7
jkl851 <jkl851@naver.com>
2021-11-19 11:10:23 +0900

    SPA api #finish

--- mysite/routes/guestbook-api.js
+++ mysite/routes/guestbook-api.js
@@ -3,6 +3,8 @@ const controller = require('../controllers/guestbook-api');
 
 const router = express.Router();
 router.route('').get(controller.read);
+router.route('').post(controller.create);
+router.route('/:no').delete(controller.delete);
 
 
 module.exports = router;
