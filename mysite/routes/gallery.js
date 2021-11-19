4fdcd47e5f667bb54efcf35018c8231edb51ec14
jkl851 <jkl851@naver.com>
2021-11-19 11:23:40 +0900

    gallery #1

---
+++ mysite/routes/gallery.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const controller = require('../controllers/gallery');
+
+const router = express.Router();
+router.route('').get(controller.index);
+
+
+
+module.exports = router;
